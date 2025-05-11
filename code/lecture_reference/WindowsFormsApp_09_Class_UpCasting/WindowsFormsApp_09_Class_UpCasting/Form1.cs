9883665ed0473283406c6fdaa475e079168f5561
Damon0527 <damon@spreatics.com>
2025-05-12 00:58:36 +0900

    lecture ready: 250512

--- code/lecture_reference/WindowsFormsApp_09_Class_UpCasting/WindowsFormsApp_09_Class_UpCasting/Form1.cs
+++ code/lecture_reference/WindowsFormsApp_09_Class_UpCasting/WindowsFormsApp_09_Class_UpCasting/Form1.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApp_09_Class_UpCasting
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); 
             // #1. 자식 객체 생성
             Sword sword = new Sword();
             sword.Name = "롱소드";
@@ -23,7 +23,59 @@ namespace WindowsFormsApp_09_Class_UpCasting
 
             Console.WriteLine("===========================");
 
-            // *업캐스팅: 자식(Sword)을 부모(Weapon) 타입으로 참조.
+            // #2. 업캐스팅: 자식(Sword)을 부모(Weapon) 타입으로 참조.
+            // Why?
+            // 다형성을 통해 여러 자식 객체를 부모 타입 하나로 통합하여 유연하고 재사용 가능한 코드로 작성하기 위해
+
+            // #3. 업캐스팅 방식 3가지
+            // 1) 묵시적 업캐스팅 (중간 변수 활용) 
+            Weapon weapon1 = sword;
+            weapon1.Attack(); // 부모 메서드만 사용 가능
+            //weapon1.Sharp(); // ❌ 에러! 부모에는 Sharp()가 없음
+
+            // 2) 다이렉트(=묵시적) 업캐스팅 (바로 대입) - 가장 많이 사용! 
+            // -[참고] 이 또한 묵시적 업캐스팅 (구별하기 위해 비공식 용어)
+            Weapon weapon2 = new Gun();
+            weapon2.Name = "AK-47";
+            weapon2.Attack();
+
+            // 3) 명시적 업캐스팅 (형변환 기호 사용) - 잘 안씀.
+            Weapon weapon3 = (Weapon)new Bow();
+            weapon3.Name = "강철 활";
+            weapon3.Attack();
+
+            Console.ReadLine();
+            Console.WriteLine("===========================");
+            // #4. 추가 내용 적용
+
+            // 검 생성
+            Sword sword2 = new Sword(30); // damage = 30
+            sword2.Name = "롱소드";
+            sword2.Attack();
+            Console.WriteLine($"검 공격 데미지: {sword.Slash(1)}"); // 자식 메서드 호출
+            Console.WriteLine("===========================");
+            // 업캐스팅
+            Weapon weapon4 = sword2;    // Sword -> Weapon 업캐스팅
+            weapon4.Attack();           // 부모 메서드 호출 가능 (o)
+            //weapon4.Slash(1);           // 자식 메서드 사용 불가 (x)
+
+            // "다운캐스팅" 해서 다시 사용 가능
+            // ㄴ 권장하지 않음!
+            // ㄴ 다운캐스팅은 실제 타입을 정확히 알고 있을 때는 안전!
+            // ㄴ 다운캐스팅은 실행 중 타입이 맞지 않으면 예외(오류)가 발생하기 때문에,
+            // ㄴ 안정성이 떨어지며 유지보수와 확장성에도 불리함.
+            // [참고] 이 방법 말고도 is, as키워드 사용해서 하는 방법있는데 굳이 ㄴㄴ
+            // [참고] 다운캐스팅 하지 않고 자식 기능 호출 가능한 방법을 배울 것임 (virtual + override)
+            Console.WriteLine($"검 공격 데미지: {((Sword)weapon4).Slash(1)}"); // 자식 메서드 호출
+            Console.WriteLine("===========================");
+
+            // 인스턴스를 덮어 씌우는 것으로 자식 클래스를 변경 가능.
+            weapon4 = new Gun(100); // 실제 객체는 Gun
+            weapon4.Name = "강철 활";
+            weapon4.Attack();
+            Console.WriteLine($"활 공격 데미지: {((Gun)weapon4).Fire(10)}");
+
+            
         }
     }
 }
--- code/lecture_reference/WindowsFormsApp_09_Class_UpCasting/WindowsFormsApp_09_Class_UpCasting/Weapon.cs
+++ code/lecture_reference/WindowsFormsApp_09_Class_UpCasting/WindowsFormsApp_09_Class_UpCasting/Weapon.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +11,21 @@ namespace WindowsFormsApp_09_Class_UpCasting
     public class Weapon
     {
         // 필드
-        private string name;
+        //private string name;
+
+        // [추가] 자식 클래스에서 접근 가능함.
+        protected int damage; 
 
         // 속성
         public string Name { get; set; }
+
+        // [추가] 생성자
+        public Weapon() { }
+
+        public Weapon(int damage)
+        {
+            this.damage = damage;
+        }
 
         // 메서드
         public void Attack()
@@ -21,6 +33,8 @@ namespace WindowsFormsApp_09_Class_UpCasting
             Console.WriteLine($"{Name} (으)로 공격!");
         }
 
+        // [추가] 데미지 리턴 메서드 (람다식)
+        public int AttackPower() => damage;
     }
     #endregion
 
@@ -28,24 +42,68 @@ namespace WindowsFormsApp_09_Class_UpCasting
     // 자식 클래스: Sword (검)
     public class Sword : Weapon
     {
+        // [추가] 공격 사거리
+        int attack_range = 1;
+
+        // [추가] 생성자
+        public Sword() { }
+
+        // base(...)를 통해 부모 생성자에게 전달.
+        public Sword(int damage) : base(damage) { }
+
+
         public void Sharp()
         {
             Console.WriteLine("검이 날카롭게 빛납니다.");
         }
+
+        // [추가] 범위 내 공격 여부 확인 후 데미지 리턴
+        public int Slash(int range)
+        {
+            // 공격 사거리가 공격할 거리보다 같거나 길으면 = 공격 성공
+            if (this.attack_range >= range)
+            {
+                return this.damage;
+            }
+            return 0;
+            // return 0; : 운영체제에게 "정상적으로 잘 끝났다~"라고 신호주는 것.
+            // return -1, 1; : 문제가 있다~ 라는 뜻.
+        }
     }
 
     // 자식 클래스: Gun (총)
-    class Gun : Weapon
+    public class Gun : Weapon
     {
+        // [추가] 공격 범위
+        int attack_range = 10;
+
+        // [추가] 생성자
+        public Gun() { }
+
+        public Gun(int damage) : base(damage) { }
+
         public void Reload()
         {
             Console.WriteLine("총을 재장전 합니다");
+        }
+
+        // [추가]
+        public int Fire(int range)
+        {
+            if (this.attack_range >= range)
+            {
+                return this.damage;
+            }
+            return 0;
         }
     }
 
     // 자식 클래스: Bow (활)
     class Bow : Weapon
     {
+        // [추가] 생성자
+        public Bow() { }
+        public Bow(int damage) : base(damage) { } 
         public void Aim()
         {
             Console.WriteLine("활을 당기며 조준합니다.");
