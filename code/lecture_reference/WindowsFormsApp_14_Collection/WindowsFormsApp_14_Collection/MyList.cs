6522743da708f473795a9f22c332700952720454
Damon0527 <damon@spreatics.com>
2025-05-15 03:39:06 +0900

    lecture ready: 250515

---
+++ code/lecture_reference/WindowsFormsApp_14_Collection/WindowsFormsApp_14_Collection/MyList.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_14_Collection
+{
+    // 제네릭에서 <T>는 **형식 매개변수(Type Parameter)**라고 부르며,
+    // ㄴ <T>: 아직 정해지지 않은 타입.
+    // ㄴ 클래스나 메서드를 선언할 때 "어떤 타입을 쓸지 나중에 지정하겠다"는 뜻으로 사용.
+    // 즉, 어떤 타입이든 지정 가능한 자리
+
+    // #1. List<T>
+    public class MyList<T>
+    {
+        private List<T> myList = new List<T>();
+
+        public void AddItem(T item)
+        {
+            myList.Add(item);
+        }
+
+        public void ShowAll()
+        {
+            foreach (T item in myList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    // #2. Stack<T>
+    public class MyStack<T>
+    {
+        private Stack<T> myStack = new Stack<T>();
+
+        public void PushItem(T item)
+        {
+            myStack.Push(item);
+        }
+
+        public T PopItem()
+        {
+            return myStack.Pop();
+        }
+
+        public T PeekItem()
+        {
+            return myStack.Peek();
+        }
+    }
+
+    // #3. Queue<T>
+    public class MyQueue<T>
+    {
+        private Queue<T> myQueue = new Queue<T>();
+
+        public void EnqueueItem(T item) // 외부에서 전달 받는 값의 타입도 T
+        {
+            myQueue.Enqueue(item);
+        }
+
+        public T DequeueItem()  // 반환 타입 T: 꺼내는 값도 T 타입.
+        {
+            return myQueue.Dequeue();
+        }
+
+        public T PeekItem()
+        {
+            return myQueue.Peek();
+        }
+    }
+
+    // #4. Dicitonary<TKey, TValue>
+    public class MyDictionary<TKey, TValue>
+    {
+        private Dictionary<TKey, TValue> dict = new Dictionary<TKey, TValue>();
+
+        public void AddItem(TKey key, TValue value)
+        {
+            dict[key] = value;  // [] 인덱서로 바로 접근 가능
+            // dict.Add(key, value);
+            
+        }
+
+        // 2. 키로 값 가져오기
+        public TValue GetItem(TKey key)
+        {
+            return dict[key];
+        }
+
+        // 3. 안전하게 값 가져오기 (TryGetValue 사용)
+        public bool TryGetItem(TKey key, out TValue value)
+        {
+            return dict.TryGetValue(key, out value);
+        }
+
+        // 4. 특정 키가 존재하는지 확인
+        public bool ContainsKey(TKey key)
+        {
+            return dict.ContainsKey(key);
+        }
+
+       
+        // 5. 항목 삭제
+        public bool RemoveItem(TKey key)
+        {
+            return dict.Remove(key);
+        }
+        // 6. 전체 출력
+        public void ShowAll()
+        {
+            //foreach (KeyValuePair<TKey, TValue> pair in dict)
+            foreach (var pair in dict) // var : 타입 자동 추론 (편의성)
+
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+
+        // 7. count 출력
+        public int Count => dict.Count;
+    }
+}
---
+++ code/pratice/WindowsFormsApp_14_Collection_Ex/WindowsFormsApp_14_Collection_Ex/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsApp_14_Collection_Ex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_file = new System.Windows.Forms.Button();
+            this.button_login = new System.Windows.Forms.Button();
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.textBox_pw = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_file
+            // 
+            this.button_file.Location = new System.Drawing.Point(12, 12);
+            this.button_file.Name = "button_file";
+            this.button_file.Size = new System.Drawing.Size(116, 46);
+            this.button_file.TabIndex = 0;
+            this.button_file.Text = "파일 열기";
+            this.button_file.UseVisualStyleBackColor = true;
+            this.button_file.Click += new System.EventHandler(this.button_file_Click);
+            // 
+            // button_login
+            // 
+            this.button_login.Location = new System.Drawing.Point(374, 101);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(113, 56);
+            this.button_login.TabIndex = 1;
+            this.button_login.Text = "로그인";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.Location = new System.Drawing.Point(173, 101);
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.Size = new System.Drawing.Size(178, 21);
+            this.textBox_id.TabIndex = 2;
+            // 
+            // textBox_pw
+            // 
+            this.textBox_pw.Location = new System.Drawing.Point(173, 137);
+            this.textBox_pw.Name = "textBox_pw";
+            this.textBox_pw.Size = new System.Drawing.Size(178, 21);
+            this.textBox_pw.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gulim", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(119, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gulim", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(119, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PW";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 205);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_pw);
+            this.Controls.Add(this.textBox_id);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.button_file);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_file;
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.TextBox textBox_id;
+        private System.Windows.Forms.TextBox textBox_pw;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
